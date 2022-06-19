a99ab6d9467729654628ac21664b7ad594723d40
hanifarhanz <52771731+hanfarhanzoo@users.noreply.github.com>
2022-06-19 13:06:54 +0700

    update init.js

--- init.js
+++ init.js
@@ -1,179 +1,168 @@
 // declaring element
-const username = document.getElementById("username");
-const registerForm = document.getElementById("registerForm");
-const logoutForm = document.getElementById("logoutForm");
-const startSection = document.getElementById("start");
+const username = document.getElementById("username")
+const registerForm = document.getElementById("registerForm")
+const logoutForm = document.getElementById("logoutForm")
+const startSection = document.getElementById("start")
 //declare box level-easy
-const boxE1 = document.getElementById("boxE1");
-const boxE2 = document.getElementById("boxE2");
-const boxE3 = document.getElementById("boxE3");
+const level_easy = 3
+const boxE1 = document.getElementById("boxE1")
+const boxE2 = document.getElementById("boxE2")
+const boxE3 = document.getElementById("boxE3")
 //declare box level-medium
-const boxM1 = document.getElementById("boxM1");
-const boxM2 = document.getElementById("boxM2");
-const boxM3 = document.getElementById("boxM3");
-const boxM4 = document.getElementById("boxM4");
+const level_medium = 4
+const boxM1 = document.getElementById("boxM1")
+const boxM2 = document.getElementById("boxM2")
+const boxM3 = document.getElementById("boxM3")
+const boxM4 = document.getElementById("boxM4")
 //declare box level-hard
-const boxH1 = document.getElementById("boxH1");
-const boxH2 = document.getElementById("boxH2");
-const boxH3 = document.getElementById("boxH3");
-const boxH4 = document.getElementById("boxH4");
-const boxH5 = document.getElementById("boxH5");
+const level_hard = 5
+const boxH1 = document.getElementById("boxH1")
+const boxH2 = document.getElementById("boxH2")
+const boxH3 = document.getElementById("boxH3")
+const boxH4 = document.getElementById("boxH4")
+const boxH5 = document.getElementById("boxH5")
 
-let difficulty = sessionStorage.getItem("keyDifficulty");
+let difficulty = sessionStorage.getItem('keyDifficulty')
 
-const rewardImage = document.getElementById("imgReward");
-const title = document.getElementById("title");
-const rewardSection = document.getElementById("reward");
-const navbar = document.getElementById("navbar");
-const levelEasy = document.getElementById("level-easy");
-const levelMedium = document.getElementById("level-medium");
-const levelHard = document.getElementById("level-hard");
-const stopButton = document.getElementById("stop");
-const nameAndToken = document.getElementById("nameAndToken");
+const rewardImage = document.getElementById("imgReward")
+const title = document.getElementById("title")
+const rewardSection = document.getElementById("reward")
+const navbar = document.getElementById("navbar")
+const levelEasy = document.getElementById("level-easy")
+const levelMedium = document.getElementById("level-medium")
+const levelHard = document.getElementById("level-hard")
+const stopButton = document.getElementById("stop")
+const nameAndToken = document.getElementById("nameAndToken")
 
-const player = new Player();
 
-let default_option = ["😍", "🤣", "😱"];
-boxE1.textContent = default_option[0];
-boxE2.textContent = default_option[1];
-boxE3.textContent = default_option[2];
+const player = new Player()
 
-boxM1.textContent = default_option[0];
-boxM2.textContent = default_option[1];
-boxM3.textContent = default_option[2];
-boxM3.textContent = default_option[0];
 
-boxH1.textContent = default_option[0];
-boxH2.textContent = default_option[1];
-boxH3.textContent = default_option[2];
-boxH4.textContent = default_option[0];
-boxH5.textContent = default_option[1];
+let default_option = ['😍', '🤣', '😱','😒','❤️']
+boxE1.textContent = default_option[0]
+boxE2.textContent = default_option[1]
+boxE3.textContent = default_option[2]
+boxM1.textContent = default_option[0]
+boxM2.textContent = default_option[1]
+boxM3.textContent = default_option[2]
+boxM4.textContent = default_option[3]
+boxH1.textContent = default_option[0]
+boxH2.textContent = default_option[1]
+boxH3.textContent = default_option[2]
+boxH4.textContent = default_option[3]
+boxH5.textContent = default_option[4]
 
 function dice() {
-  let gacha = [];
+  let gacha = []
   for (let i = 0; i < default_option.length; i++) {
-    const roll = default_option[~~(Math.random() * default_option.length)];
-    gacha.push(roll);
+    const roll = default_option[~~(Math.random() * default_option.length)]
+    gacha.push(roll)
   }
-  return gacha;
+  return gacha
 }
 
 function reward() {
-  fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
-    .then((x) => x.json())
-    .then((result) => {
-      //set nama hadiah reward
-      let text = document.createElement("h1");
-      text.textContent = result.name;
+  fetch('https://zoo-animal-api.herokuapp.com/animals/rand').then(x => x.json()).then(result => {
 
-      //set gambar hadiah
-      let img = new Image(200, 200);
-      img.src = result.image_link;
+    //set nama hadiah reward
+    let text = document.createElement('h1')
+    text.textContent = result.name
 
-      //set element
-      rewardImage.appendChild(img);
-      rewardImage.appendChild(text);
-    });
+    //set gambar hadiah
+    let img = new Image(200, 200)
+    img.src = result.image_link
+
+    //set element
+    rewardImage.appendChild(img)
+    rewardImage.appendChild(text)
+  })
 }
 
 function winner() {
-  if (
-    boxE1.textContent == boxE2.textContent &&
-    boxE1.textContent == boxE3.textContent
-  ) {
-    location.href = "#reward";
-    reward();
+  if (boxE1.textContent == boxE2.textContent && boxE1.textContent == boxE3.textContent) {
+    location.href = "#reward"
+    reward()
   } else {
-    alert("Anda kalah! silakan coba lagi.");
-    console.log("lose");
+    alert('Anda kalah! silakan coba lagi.')
+    console.log('lose')
   }
 }
 
 function start() {
-  //selama
-  const rolling = setInterval(function () {
-    const result = dice();
-    boxE1.textContent = result[0];
-    boxE2.textContent = result[1];
-    boxE3.textContent = result[2];
-
-    boxM1.textContent = default_option[0];
-    boxM2.textContent = default_option[1];
-    boxM3.textContent = default_option[2];
-    boxM4.textContent = default_option[0];
-
-    boxH1.textContent = default_option[0];
-    boxH2.textContent = default_option[1];
-    boxH3.textContent = default_option[2];
-    boxH4.textContent = default_option[0];
-    boxH5.textContent = default_option[1];
-  }, 100);
+  
+    //selama
+    const rolling = setInterval(function () {
+      const result = dice()
+      boxE1.textContent = result[0]
+      boxE2.textContent = result[1]
+      boxE3.textContent = result[2]
+    }, 100)
 
   //ketika
-  setTimeout(function () {
-    clearInterval(rolling);
-    winner();
-  }, 5000);
+    setTimeout(function () {
+      clearInterval(rolling)
+      winner()
+    }, 5000)
 }
 
-function stop() {
-  clearInterval(rolling);
-  winner();
-}
+  function stop(){
+    clearInterval(rolling)
+    winner()
+  }
 
 onload = function () {
-  const token = sessionStorage.getItem("keyToken");
-  const username = sessionStorage.getItem("keyUsername");
-  levelMedium.style.display = "none";
-  levelHard.style.display = "none";
+  const token = sessionStorage.getItem('keyToken')
+  const username = sessionStorage.getItem('keyUsername')
+  levelMedium.style.display = "none"
+  levelHard.style.display = "none"
 
   if (token && token != null) {
-    registerForm.style.display = "none";
-    logoutForm.style.display = "block";
-    startSection.style.display = "block";
-    rewardSection.style.display = "block";
-    navbar.style.display = "block";
-    title.textContent = `Hello ${username} !`;
-    nameAndToken.textContent = `${username} (Session Token : ${token})`;
+    registerForm.style.display = "none"
+    logoutForm.style.display = "block"
+    startSection.style.display = "block"
+    rewardSection.style.display = "block"
+    navbar.style.display = "block"
+    title.textContent = `Hello ${username} !`
+    nameAndToken.textContent= `${username} (Session Token : ${token})`
   } else {
-    registerForm.style.display = "block";
-    logoutForm.style.display = "none";
-    startSection.style.display = "none";
-    rewardSection.style.display = "none";
-    navbar.style.display = "none";
+    registerForm.style.display = "block"
+    logoutForm.style.display = "none"
+    startSection.style.display = "none"
+    rewardSection.style.display = "none"
+    navbar.style.display = "none"
   }
-};
+}
 
-function levelEasyButton() {
-  sessionStorage.setItem("keyDifficulty", "easy");
-  const difficulty = sessionStorage.getItem("keyDifficulty");
-  console.log("levelnya " + difficulty);
-  levelEasy.style.display = "flex";
-  levelMedium.style.display = "none";
-  levelHard.style.display = "none";
+function levelEasyButton(){
+  sessionStorage.setItem('keyDifficulty', "easy")
+  const difficulty = sessionStorage.getItem('keyDifficulty')
+  console.log("levelnya " + difficulty)
+  levelEasy.style.display = "flex"
+  levelMedium.style.display = "none"
+  levelHard.style.display = "none"
 }
-function levelMediumButton() {
-  sessionStorage.setItem("keyDifficulty", "medium");
-  const difficulty = sessionStorage.getItem("keyDifficulty");
-  console.log("yang ini " + difficulty);
-  levelEasy.style.display = "none";
-  levelMedium.style.display = "flex";
-  levelHard.style.display = "none";
+function levelMediumButton(){
+  sessionStorage.setItem('keyDifficulty', "medium")
+  const difficulty = sessionStorage.getItem('keyDifficulty')
+  console.log("yang ini " + difficulty)
+  levelEasy.style.display = "none"
+  levelMedium.style.display = "flex"
+  levelHard.style.display = "none"
 }
-function levelHardButton() {
-  sessionStorage.setItem("keyDifficulty", "hard");
-  const difficulty = sessionStorage.getItem("keyDifficulty");
-  console.log("nah ini " + difficulty);
-  levelEasy.style.display = "none";
-  levelMedium.style.display = "none";
-  levelHard.style.display = "flex";
+function levelHardButton(){
+  sessionStorage.setItem('keyDifficulty', "hard")
+  const difficulty = sessionStorage.getItem('keyDifficulty')
+  console.log("nah ini " + difficulty)
+  levelEasy.style.display = "none"
+  levelMedium.style.display = "none"
+  levelHard.style.display = "flex"
 }
 
 function register() {
-  player.username = username.value;
-  player.register;
+  player.username = username.value
+  player.register
 }
 
 function logout() {
-  player.logout;
+  player.logout
 }
